ff198067e19aa7fd3b37b5451ead63ccca0bdae3
Ian <258510481+IanDrag11@users.noreply.github.com>
2026-09-12 20:42:34 -0400

    counting rows of csv

--- main.py
+++ main.py
@@ -4,3 +4,16 @@ def read_csv_file(filename):
             print(line.strip())
 
 read_csv_file('data.csv')
+
+def count_rows(filename):
+    with open(filename, "r") as file:
+        line_total = 0
+        for line in file:
+            line_total += 1
+        return line_total
+
+print(count_rows('data.csv'))
+
+
+
+
